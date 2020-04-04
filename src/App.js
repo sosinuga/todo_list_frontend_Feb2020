a0ca1747e58f23683fa3da4c10484fef5072ca3e
Sike Osinuga <adesikeoye@yahoo.com>
2020-04-04 22:35:46 +0100

    Delete function implemented

--- src/App.js
+++ src/App.js
@@ -1,64 +1,71 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header/Header.js";
 import TaskCount from "./TaskCount/TaskCount.js";
 import Task from "./Task/Task.js";
-import AddNewTask from "./AddTask.js";
+import AddNewTask from "./AddNewTask";
 
 
 
 function App() {
-  // const [counter, setCounter] = useState(0);
 
-  //   function increaseCounterBy1() {
-  //     setCounter(counter + 1);
-  //   }
-  //   function decreaseCounterBy1() {
-  //     if(counter >0){
-  //     setCounter(counter - 1);
-  //   }
-  // }
 
   const [tasks, setTasks] = useState([
     {
       text: "Pay Bills",
       completed: true,
       dueDate: "2020-03-31",
-      urgent: true
+      urgent: true,
+      id: 1
     },
     {
       text: "Homework",
       completed: false,
       dueDate: "2020-03-31",
-      urgent: true
+      urgent: true,
+      id: 2
     },
     {
       text: "Buy train tickets",
       completed: true,
       dueDate: "2020-04-06",
-      urgent: false
+      urgent: false,
+      id: 3
     },
     {
       text: "Return books to the library",
       completed: false,
       dueDate: "2020-04-02",
-      urgent: false
+      urgent: false,
+      id: 4
     },
     {
       text: "Do laundry",
       completed: true,
       dueDate: "2020-03-30",
-      urgent: true
+      urgent: true,
+      id: 5
     },
     {
       text: "Call dad",
       completed: true,
       dueDate: "2020-04-02",
-      urgent: false
+      urgent: false,
+      id: 6
     }
   ]);
+  // function that deletes a task from the task array, and updates the state with the new array
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter((task) => {
+if (task.id === id) {
+  return false;
+} else{
+  return true;
+}
+    });
 
+    setTasks(filteredTasks);
+  }
   return (
     <div className="App">
 
@@ -67,15 +74,20 @@ function App() {
 
         <TaskCount count={tasks.length} />
         <div className="container">
+          <AddNewTask />
           {/* Passing a prop of text to each Task component */}
           {tasks.map((task) => {
             return (
-            <Task 
-            text={task.text} 
-            urgent={task.urgent} 
-            completed={task.completed} 
-            dueDate={task.dueDate}
-            />
+              <Task
+                key={task.id}
+
+                deleteTaskFunc={deleteTask}
+                text={task.text}
+                urgent={task.urgent}
+                completed={task.completed}
+                dueDate={task.dueDate}
+                id={task.id}
+              />
             );
           })}
 
--- src/Task/Task.js
+++ src/Task/Task.js
@@ -2,16 +2,28 @@ import React from "react";
 import moment from "moment";
 import "./Task.css";
 
-function Task(props) {
-  // props = { text: "Do the dishes", urgent: true, completed: true, dueDate: "2020-03-10" }
+
+const Task = (props) => {
+  // props = { text: "Do the dishes", urgent: true, completed: true, dueDate: "2020-03-10", id: 2. deleteTaskFunc: fn() }
+  const handleClick = () => {
+    console.log("Delete button clicked");
+    console.log(props.id);
+    props.deleteTaskFunc(props.id);
+  }
   return (
     <div className="row taskRow">
-      <div className="col-12 col-md-4">{props.text}</div>
-      <div className="col-6 col-md-2">{props.urgent === true ? "Urgent" : ""}</div>
-    <div className="col -12">{"Due by "}{props.dueDate}</div>
-      <div className="col-6 col-md-2">{props.completed === false && <button className="btn btn-primary">Mark as complete</button>}</div>
+      <div className="col-12 col-md-4">{props.text}
+      </div>
+      <div className="col-6 col-md-2">{props.urgent === true ? "Urgent" : ""}
+      </div>
+      <div className="col-6 col-md-2">{"Due by "}{moment(props.dueDate).format('ddd Do MMMM YYYY')}
+      </div>
       <div className="col-6 col-md-2">
-        <button className="btn btn-danger">Delete</button>
+      {props.completed === false && <button className="btn btn-primary">Mark as complete</button>}
+      </div>
+      <div className="col-6 col-md-2">
+       
+  <button className="btn btn-danger" onClick={handleClick}>Delete</button>
       </div>
     </div>
   );
