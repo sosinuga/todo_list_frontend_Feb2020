ce8ced518c7d8f1fc526b7e9dccd17c7e47575f9
Sike Osinuga <adesikeoye@yahoo.com>
2020-04-25 16:26:04 +0100

    Updates

---
+++ precache-manifest.2d3039a51f29cd03e64d6b578a3fe8df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5d0e1a4bcb0f208c854e1184863add",
+    "url": "/todo_list_frontend_Feb2020/index.html"
+  },
+  {
+    "revision": "7c7dccbf065f48b8f24d",
+    "url": "/todo_list_frontend_Feb2020/static/css/main.657003be.chunk.css"
+  },
+  {
+    "revision": "0ae2d9c7ce20a7dcba07",
+    "url": "/todo_list_frontend_Feb2020/static/js/2.6f2fdb7d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo_list_frontend_Feb2020/static/js/2.6f2fdb7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7dccbf065f48b8f24d",
+    "url": "/todo_list_frontend_Feb2020/static/js/main.e7ae0ce7.chunk.js"
+  },
+  {
+    "revision": "5bec38460c0b73a1662b",
+    "url": "/todo_list_frontend_Feb2020/static/js/runtime-main.71ced4e7.js"
+  }
+]);
